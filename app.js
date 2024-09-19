49e5b114cfee9a0f53c9516b1c9b13e8afe6c380
Tarun Patil <tarunpatil001@gmail.com>
2024-09-19 17:31:06 +0530

    Delete operation implemented.

--- app.js
+++ app.js
@@ -64,7 +64,15 @@ app.get("/listings/:id/edit", async (req, res)=>{
 app.put("/listings/:id", async (req, res)=>{
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
+});
+
+// Delete Route
+app.delete("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req, res)=>{
